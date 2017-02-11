0bd47201543c9fee237704c4176fd89e500a5d25
Thies Mueller <github@td00.de>
2017-02-11 19:12:31 +0100

    fixed spelling BONUS

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 	for (i=0; i<24; i++)	{
 	
 		if (i==12) {
-			$('#board').append("<div data-value='1' class='selected freesquare' id='sqfree'><div class='text'><br/>BONUBONUS</div>");
+			$('#board').append("<div data-value='1' class='selected freesquare' id='sqfree'><div class='text'><br/>BONUS</div>");
 			$('#board').append("<div data-value='0' class='square' id='sq12'><div class='text'><br/>"+JSONBingo.squares[i].square+"</div></div>");
 		} else {
 			$('#board').append("<div data-value='0' class='square' id='sq"+i+"'><div class='text'><br/>"+JSONBingo.squares[i].square+"</div></div>");
