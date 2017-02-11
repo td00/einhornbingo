0c5a4e403d42595616a1fa1b30c915ea9f3d5c85
Thies Mueller <github@td00.de>
2017-02-11 19:06:51 +0100

    push text

--- js/data.js
+++ js/data.js
@@ -1,38 +1,38 @@
 
-var headerText = "Bingo";
+var headerText = "Einhornbingo";
 
-var footerText = "<a href='https://github.com/jeffehobbs/HTML5-bingo/' target='_blank'>Code available on github</a>";
+var footerText = "<a href='https://github.com/td00/einhornbingo/' target='_blank'>Code available on github</a>";
 
-var winText = "Winner";
+var winText = "Gewonnen";
 
 var clickSnd = new Audio("audio/click.mp3");
 
 var winSnd = new Audio("audio/win.mp3");
 
 var JSONBingo = {"squares": [
-        {"square": "one"},
-        {"square": "two"},
-        {"square": "three"},
-        {"square": "four"},
-        {"square": "five"},
-        {"square": "six"},
-        {"square": "seven"},
-        {"square": "eight"},
-        {"square": "nine"},
-        {"square": "ten"},
-        {"square": "eleven"},
-        {"square": "twelve"},
-        {"square": "thirteen"},
-        {"square": "fourteen"},
-        {"square": "fifteen"},
-        {"square": "sixteen"},
-        {"square": "seventeen"},
-        {"square": "eighteen"},
-        {"square": "nineteen"},
-        {"square": "twenty"},
-        {"square": "twenty one"},
-        {"square": "twenty two"},
-        {"square": "twenty three"},
+        {"square": "Wer hat Pizza bestellt?"},
+        {"square": "Vorsicht Stufe!"},
+        {"square": "Wo ist das Frauenklo?"},
+        {"square": "Du kannst vorbei, durch tut weh!"},
+        {"square": "*Jemand rennt gegen die Lampe*"},
+        {"square": "Neu hier?"},
+        {"square": "Ich bin nur kurz auf 1 Bier hier!"},
+        {"square": "*Ein Gast rauemt den Tisch ab*"},
+        {"square": "*Der Barkeeper lacht*"},
+        {"square": "Eine Blackpearl bitte"},
+        {"square": "Willst du ne Nummer oder gleich zahlen?"},
+        {"square": "*Kolja flirtet*"},
+        {"square": "*Stefan spielt den selben Song*"},
+        {"square": "*Jemand spielt nen Hegenberg Song*"},
+        {"square": "Persokontrolle"},
+        {"square": "Ich rauche jetzt die Kippe of Destiny"},
+        {"square": "*Jan hat Kippen*Super Rare"},
+        {"square": "FENSTER AUF!"},
+        {"square": "Fluch Nummer 2 ist scheisse!"},
+        {"square": "Hat jemand nen Stift? Ich will auch was an die Decke schreiben!"},
+        {"square": "Einen Blindschuss bitte"},
+        {"square": "1 Met und X Becher bitte"},
+        {"square": "Leute nicht so laut, denkt an die Nachbarn"},
         {"square": "twenty four"},    	
         {"square": "twenty five"},    	
         {"square": "twenty six"},    	
